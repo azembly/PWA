cb85bb8a6d30a216afaf0fb39b67fb39fa0f5205
Elliot Lundemann O'Connor <5060405@RTS-365.dk>
2021-01-26 13:17:35 +0100

    inital commit!

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const cacheName = "shell-content-v6"
+const filesToCache = [
+    '/style.css',
+    '/logo192.png',
+    '/script.js',
+    '/index.html',
+    '/offline.html'
+]
+
+self.addEventListener("install", evt => {
+    console.log("the service worker was installed!");
+    evt.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                console.log("Caching app shell")
+                return cache.addAll(filesToCache)
+            })
+    );
+});
+
+self.addEventListener("activate", evt => {
+    console.log("the service worker was activated");
+    evt.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                console.log(cacheNames);
+                return Promise.all(
+                    cacheNames.filter(oldCache => cacheName !== oldCache)
+                    .map(cacheName => caches.delete(cacheName))
+                )
+            })
+    )
+});
+
+// Cache-first...
+self.addEventListener("fetch", evt => {
+    evt.respondWith(caches.match(evt.request)
+        .then(respone => {
+            return response || fetch(evt.request)
+        })
+        .catch(err => caches.match('offline.html'))
+        )
+})
